67648c1a52495b4f0ace7d800256db500cbcdfdf
ruzz-gd <ruzhell0909@gmail.com>
2024-10-05 19:53:25 +0800

    update

--- php-example/db-config.php
+++ php-example/db-config.php
@@ -1,10 +1,8 @@
 <?php
-
-$host = 'localhost';
-$dbname = 'pogi_team_db';
-$username = 'root';  
-$password = '';     
-
+$host = '127.0.0.1'; 
+$dbname = '';# database name 
+$username = '';# username or root 
+$password = '';# your_password    
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
