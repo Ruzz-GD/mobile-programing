075ce3b6310134f53801d63ea16f7ebdb9807eb2
ruzz-gd <ruzhell0909@gmail.com>
2024-10-06 21:48:14 +0800

    update

--- php-example/db-config.php
+++ php-example/db-config.php
@@ -1,8 +1,8 @@
 <?php
 $host = '127.0.0.1'; 
-$dbname = '';# database name 
-$username = '';# username or root 
-$password = '';# your_password    
+$dbname = 'my_database';# database name 
+$username = 'ruzz';# username or root 
+$password = 'ruzz2323';# your_password    
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
